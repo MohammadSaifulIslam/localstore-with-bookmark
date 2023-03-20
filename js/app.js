75bfe6570016e4c9c093061051ef0e4e4a0f28b6
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-03-20 06:48:09 +0600

    handleBookmark function added

--- js/app.js
+++ js/app.js
@@ -15,6 +15,10 @@ const showProduct = data => {
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-100">
+        <div class="d-flex p-3 flex-row-reverse gap-2 fs-4 btn">
+            <i onclick="handleBookmark('${id}', '${name}', '${price}')" class="fa-solid fa-bookmark"></i>
+            <i class="fa-regular fa-bookmark "></i>
+        </div>
             <img src="${image}" class="p-3 img-fluid" alt="..." style="height: 300px;">
             <div class="card-body">
                 <h2 class="card-title">${name}</h2>
@@ -27,4 +31,30 @@ const showProduct = data => {
     });
 }
 
+const handleBookmark = (id, name, price) => {
+    const previousBookmark = JSON.parse(localStorage.getItem('bookmark'));
+    let bookmark = [];
+    console.log(bookmark)
+    const product = { id, name, price, bookmark: true }
+    if (previousBookmark) {
+        const isThisProductBookmark = previousBookmark.find(pd => pd.id == id);
+        if (isThisProductBookmark) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'This product is already bookmark',
+            })
+        } else {
+            bookmark.push(...previousBookmark, product)
+            localStorage.setItem('bookmark', JSON.stringify(bookmark))
+        }
+
+        // localStorage.getItem('bookmark')
+    } else {
+        bookmark.push(product)
+        localStorage.setItem('bookmark', JSON.stringify(bookmark))
+    }
+
+
+}
 loadProduct()
